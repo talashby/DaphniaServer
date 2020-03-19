3c955a7c489fb7c45317bac9d8b4ab71e9f5cc5e
talashby <ukhov@mail.ru>
2020-03-19 12:52:24 +0300

    server

--- DaphniaServer/DaphniaServer.cpp
+++ DaphniaServer/DaphniaServer.cpp
@@ -2,14 +2,24 @@
 #undef UNICODE
 
 #include "ParallelPhysics.h"
+#include <iostream>
 
 
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
 
-int __cdecl main(void)
+int main(int argc, char** argv)
 {
-	PPh::ParallelPhysics::Init(PPh::VectorInt32Math(), 0);
+	if (argc < 3)
+	{
+		std::cout << "Not enough arguments";
+	}
+	PPh::VectorInt32Math size;
+	size.m_posX = std::atoi(argv[0]);
+	size.m_posY = std::atoi(argv[1]);
+	size.m_posZ = std::atoi(argv[2]);
+
+	PPh::ParallelPhysics::Init(size, 0);
 	PPh::ParallelPhysics::GetInstance()->StartSimulation();
 	return 0;
 }
